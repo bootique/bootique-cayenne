c5396ca5a299c196b139b52d0894d67c33363fec
Elena Bondareva <ebondareva@objectstyle.com>
2017-08-15 18:16:03 +0300

    bootique-cayenne-test presence breaks BQModuleProviderChecker.testMetadata #38: fix ignored test

--- bootique-cayenne-test/src/test/java/io/bootique/cayenne/test/CayenneTestModuleIT.java
+++ bootique-cayenne-test/src/test/java/io/bootique/cayenne/test/CayenneTestModuleIT.java
@@ -1,8 +1,12 @@
 package io.bootique.cayenne.test;
 
+import io.bootique.BQRuntime;
+import io.bootique.cayenne.test.persistence.Table1;
+import io.bootique.cayenne.test.persistence.Table2;
 import io.bootique.test.junit.BQTestFactory;
 import org.apache.cayenne.map.DataMap;
-import org.junit.Ignore;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -12,23 +16,26 @@ import static org.mockito.Mockito.verify;
 
 public class CayenneTestModuleIT {
 
+    @ClassRule
+    public static BQTestFactory TEST_FACTORY = new BQTestFactory();
+    private static BQRuntime TEST_RUNTIME;
+
     @Rule
-    public BQTestFactory testFactory = new BQTestFactory();
+    public CayenneTestDataManager dataManager = new CayenneTestDataManager(TEST_RUNTIME, true, Table1.class, Table2.class);
+    private static SchemaListener listener = mock(SchemaListener.class);
 
-    @Test
-    @Ignore
-    public void testSchemaListeners() {
-
-        SchemaListener listener = mock(SchemaListener.class);
-
-        testFactory.app("-c", "classpath:config2.yml")
+    @BeforeClass
+    public static void beforeClass() {
+        TEST_RUNTIME = TEST_FACTORY.app("-c", "classpath:config2.yml")
                 .autoLoadModules()
                 .module(b -> {
                     CayenneTestModule.contributeSchemaListener(b).addBinding().toInstance(listener);
                 })
                 .createRuntime();
+    }
 
-
+    @Test
+    public void testSchemaListeners() {
         verify(listener).afterSchemaCreated(any(DataMap.class));
     }
 
