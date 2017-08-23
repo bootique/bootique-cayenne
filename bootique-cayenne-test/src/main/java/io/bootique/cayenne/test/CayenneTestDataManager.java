5735de5513122a0c5ffdde97c1fd997a4b58240e
Andrus Adamchik <andrus@objectstyle.com>
2017-08-23 10:07:32 +0300

    Support for join tables in CayenneTestDataManager #45
    * refactoring CayenneTestDataManager construction to a builder

--- bootique-cayenne-test/src/main/java/io/bootique/cayenne/test/CayenneTestDataManager.java
+++ bootique-cayenne-test/src/main/java/io/bootique/cayenne/test/CayenneTestDataManager.java
@@ -24,9 +24,38 @@ public class CayenneTestDataManager extends TestDataManager {
 
     private CayenneTableManager tableManager;
 
+    /**
+     * Creates a builder of CayenneTestDataManager
+     *
+     * @param runtime {@link BQRuntime} used in the test.
+     * @return a new instance of CayenneTestDataManager builder.
+     * @since 0.24
+     */
+    public static Builder builder(BQRuntime runtime) {
+        return new Builder(runtime);
+    }
+
+    /**
+     * @param runtime
+     * @param deleteData
+     * @param entityTypes
+     * @deprecated since 0.24 use {@link #builder(BQRuntime)} to create the data manager.
+     */
+    @Deprecated
     public CayenneTestDataManager(BQRuntime runtime, boolean deleteData, Class<?>... entityTypes) {
         super(deleteData, tablesInInsertOrder(runtime, entityTypes));
         this.tableManager = runtime.getInstance(CayenneTableManager.class);
+    }
+
+    /**
+     * @param deleteData          whether all managed tables should be deleted before each test.
+     * @param tableManager        an object that maps Cayenne DbEntities to Tables.
+     * @param tablesInInsertOrder a subset of of all tables managed by this object.
+     * @since 0.24
+     */
+    protected CayenneTestDataManager(boolean deleteData, CayenneTableManager tableManager, Table... tablesInInsertOrder) {
+        super(deleteData, tablesInInsertOrder);
+        this.tableManager = tableManager;
     }
 
     private static Table[] tablesInInsertOrder(BQRuntime runtime, Class<?>... entityTypes) {
@@ -34,16 +63,21 @@ public class CayenneTestDataManager extends TestDataManager {
         ServerRuntime serverRuntime = runtime.getInstance(ServerRuntime.class);
         EntityResolver resolver = serverRuntime.getDataDomain().getEntityResolver();
 
-        // note: do not obtain sorter from Cayenne DI. It is not a singleton and will come
-        // uninitialized
-        EntitySorter sorter = serverRuntime.getDataDomain().getEntitySorter();
-
-
         List<DbEntity> dbEntities = new ArrayList<>();
 
         for (Class<?> type : entityTypes) {
-            dbEntities.add(getDbEntity(serverRuntime, type));
+            dbEntities.add(getDbEntity(resolver, type));
         }
+
+        return tablesInInsertOrder(runtime, dbEntities);
+    }
+
+    private static Table[] tablesInInsertOrder(BQRuntime runtime, List<DbEntity> dbEntities) {
+
+        // note: do not obtain sorter from Cayenne DI. It is not a singleton and will come
+        // uninitialized
+        ServerRuntime serverRuntime = runtime.getInstance(ServerRuntime.class);
+        EntitySorter sorter = serverRuntime.getDataDomain().getEntitySorter();
 
         sorter.sortDbEntities(dbEntities, false);
 
@@ -58,7 +92,9 @@ public class CayenneTestDataManager extends TestDataManager {
 
     public static Table createTableModel(BQRuntime runtime, Class<?> entityType) {
         ServerRuntime serverRuntime = runtime.getInstance(ServerRuntime.class);
-        return createTableModel(DatabaseChannel.get(runtime), getDbEntity(serverRuntime, entityType));
+        return createTableModel(DatabaseChannel.get(runtime), getDbEntity(
+                serverRuntime.getDataDomain().getEntityResolver(),
+                entityType));
     }
 
     public static Table createTableModel(BQRuntime runtime, String tableName) {
@@ -68,8 +104,8 @@ public class CayenneTestDataManager extends TestDataManager {
         return createTableModel(DatabaseChannel.get(runtime), dbEntity);
     }
 
-    static DbEntity getDbEntity(ServerRuntime runtime, Class<?> entityType) {
-        ObjEntity entity = runtime.getDataDomain().getEntityResolver().getObjEntity(entityType);
+    private static DbEntity getDbEntity(EntityResolver resolver, Class<?> entityType) {
+        ObjEntity entity = resolver.getObjEntity(entityType);
 
         if (entity == null) {
             throw new IllegalArgumentException("Not a Cayenne entity class: " + entityType.getName());
@@ -101,5 +137,42 @@ public class CayenneTestDataManager extends TestDataManager {
 
     public Table getTable(Class<?> entityType) {
         return tableManager.getTable(entityType);
+    }
+
+    public static class Builder {
+        private BQRuntime runtime;
+        private boolean deleteData;
+        private List<DbEntity> entities;
+        private EntityResolver resolver;
+
+        private Builder(BQRuntime runtime) {
+            this.runtime = runtime;
+
+            ServerRuntime serverRuntime = runtime.getInstance(ServerRuntime.class);
+            this.resolver = serverRuntime.getDataDomain().getEntityResolver();
+            this.entities = new ArrayList<>();
+        }
+
+        public CayenneTestDataManager build() {
+            return new CayenneTestDataManager(deleteData,
+                    runtime.getInstance(CayenneTableManager.class),
+                    tablesInInsertOrder(runtime, entities));
+        }
+
+        public Builder deleteData() {
+            this.deleteData = true;
+            return this;
+        }
+
+        public Builder entities(Class<?>... entityTypes) {
+
+            if (entityTypes != null) {
+                for (Class<?> type : entityTypes) {
+                    entities.add(getDbEntity(resolver, type));
+                }
+            }
+
+            return this;
+        }
     }
 }
