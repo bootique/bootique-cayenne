98e099867b0d02f7915576cfe253381e1294ae6a
Ruslan Ibragimov <ruslan@ibragimov.by>
2018-02-09 17:30:11 +0300

    Declare module dependencies bootique/bootique#203
    Added tests.

--- bootique-cayenne-test/src/main/java/io/bootique/cayenne/test/CayenneTestModuleProvider.java
+++ bootique-cayenne-test/src/main/java/io/bootique/cayenne/test/CayenneTestModuleProvider.java
@@ -2,6 +2,12 @@ package io.bootique.cayenne.test;
 
 import com.google.inject.Module;
 import io.bootique.BQModuleProvider;
+import io.bootique.cayenne.CayenneModuleProvider;
+import io.bootique.jdbc.test.JdbcTestModuleProvider;
+
+import java.util.Collection;
+
+import static java.util.Arrays.asList;
 
 /**
  * @since 0.17
@@ -11,5 +17,13 @@ public class CayenneTestModuleProvider implements BQModuleProvider {
     @Override
     public Module module() {
         return new CayenneTestModule();
+    }
+
+    @Override
+    public Collection<BQModuleProvider> dependencies() {
+        return asList(
+                new CayenneModuleProvider(),
+                new JdbcTestModuleProvider()
+        );
     }
 }
--- bootique-cayenne-test/src/test/java/io/bootique/cayenne/test/CayenneTestModuleProviderTest.java
+++ bootique-cayenne-test/src/test/java/io/bootique/cayenne/test/CayenneTestModuleProviderTest.java
@@ -1,12 +1,42 @@
 package io.bootique.cayenne.test;
 
+import io.bootique.BQRuntime;
+import io.bootique.cayenne.CayenneModule;
+import io.bootique.jdbc.JdbcModule;
+import io.bootique.jdbc.test.JdbcTestModule;
+import io.bootique.jdbc.tomcat.TomcatJdbcModuleProvider;
 import io.bootique.test.junit.BQModuleProviderChecker;
+import io.bootique.test.junit.BQTestFactory;
+import org.junit.Rule;
 import org.junit.Test;
 
+import static com.google.common.collect.ImmutableList.of;
+
 public class CayenneTestModuleProviderTest {
+
+    @Rule
+    public BQTestFactory testFactory = new BQTestFactory();
 
     @Test
     public void testPresentInJar() {
         BQModuleProviderChecker.testPresentInJar(CayenneTestModuleProvider.class);
+    }
+
+    @Test
+    public void testModuleDeclaresDependencies() {
+        final BQRuntime bqRuntime = testFactory
+                // add arguments and tomcat module,
+                // since DataSource required for CayenneTestModule
+                .app("-c", "classpath:config4.yml")
+                .module(new TomcatJdbcModuleProvider())
+                .module(new CayenneTestModuleProvider())
+                .createRuntime();
+
+        BQModuleProviderChecker.testModulesLoaded(bqRuntime, of(
+                JdbcModule.class,
+                JdbcTestModule.class,
+                CayenneModule.class,
+                CayenneTestModule.class
+        ));
     }
 }
