a3b5ea1c8433e9b46d717dafd1ca4c766430fdef
Andrus Adamchik <andrus@objectstyle.com>
2018-04-07 15:12:33 +0300

    deprecated API cleanup

--- bootique-cayenne-test/src/test/java/io/bootique/cayenne/test/CayenneTestModuleIT.java
+++ bootique-cayenne-test/src/test/java/io/bootique/cayenne/test/CayenneTestModuleIT.java
@@ -5,7 +5,7 @@ import org.apache.cayenne.map.DataMap;
 import org.junit.Rule;
 import org.junit.Test;
 
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
--- bootique-cayenne/src/test/java/io/bootique/cayenne/ServerRuntimeFactoryTest.java
+++ bootique-cayenne/src/test/java/io/bootique/cayenne/ServerRuntimeFactoryTest.java
@@ -13,7 +13,7 @@ import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.anyString;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
