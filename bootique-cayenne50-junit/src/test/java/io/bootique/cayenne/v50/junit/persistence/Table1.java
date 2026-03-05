e054e63bbce07896b9e6d23a9d99bf6498f5cb22
Andrus Adamchik <andrus@objectstyle.com>
2026-03-05 15:58:04 -0500

    JUnit 6 upgrade

---
+++ bootique-cayenne50-junit/src/test/java/io/bootique/cayenne/v50/junit/persistence/Table1.java
@@ -0,0 +1,9 @@
+package io.bootique.cayenne.v50.junit.persistence;
+
+import io.bootique.cayenne.v50.junit.persistence.auto._Table1;
+
+public class Table1 extends _Table1 {
+
+    private static final long serialVersionUID = 1L; 
+
+}
